203b3d8953859a78e407e25ed0eeb4b69358c99d
Richard Cruikshank <rcruiksh@uvic.ca>
2020-03-23 14:40:31 -0700

    intial scrape and retrieval of table using phantomjs and beautifulsoup

---
+++ scheduler/notifier/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+import requests, json
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the notifier index.")
+
+def scrape(request):
+    # data = requests.get("https://helpdesk.uvic.ca/tools/index.php?next_page=schedule/viewAll.php")
+    # print(str(data.text), file=open("data.html", "w"))
+
+    url = 'https://helpdesk.uvic.ca/tools/index.php?next_page=schedule/viewAll.php'
+    driver = webdriver.PhantomJS()#executable_path='~/Library/phantomjs-2.1.1-macosx/bin/phantomjs')
+    driver.get(url)
+    # print(driver.page_source)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    a = soup.find_all("table", class_="dailySchedule") 
+    print(a)
+    # print(soup.prettify())
+
+    return HttpResponse("hello")
