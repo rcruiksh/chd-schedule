c49a0e73f9697050a716bb66dd6231291bf37e11
Richard Cruikshank <rcruiksh@uvic.ca>
2020-03-24 13:25:58 -0700

    schedule for each consultant built by date. next step is to scrape future dates

--- scheduler/notifier/scraper.py
+++ scheduler/notifier/scraper.py
@@ -6,47 +6,61 @@ def scrape(request):
     # data = requests.get("https://helpdesk.uvic.ca/tools/index.php?next_page=schedule/viewAll.php")
     # print(str(data.text), file=open("data.html", "w"))
 
+    # TODO: fix var names
+    # TODO: use django model to replace data structures
+    # TODO: build schedule over multiple days
+    # TODO: compare past sched with current
+    # TODO: function for building sched first time vs not?
+
     # This is being used as a headless browser, since the schedule html is built by a JS Function
     # Just performing a get will return the page source, but does not contain the schedule html.
     # using a headless browser executes that JS and allows us to retrieve the final rendered html.
-    driver = webdriver.PhantomJS()#executable_path='~/Library/phantomjs-2.1.1-macosx/bin/phantomjs')
+    driver = webdriver.PhantomJS() # TODO: phontomJS is deprecated, replace with headless
     url = 'https://helpdesk.uvic.ca/tools/index.php?next_page=schedule/viewAll.php'
     driver.get(url)
-    # print(driver.page_source)
-
-    # BeautifulSoup is a library built for parsing html
     soup = BeautifulSoup(driver.page_source, 'html.parser')
-    a = soup.find("table", class_="dailySchedule")
-    # print(a)
-    num_consultants = 0
-    schedule = []
-    consultants = []
-    # count is how we keep track of which consultant a specific <td> belongs to
-    count = 0
-    # ignore_states = []
 
-    while(1):
-        a = a.find_next()
-        # print(a.name)
+    sched_header = soup.find("table", class_="schedule_control")
+    date = sched_header.find_next("b")
+    date = date.text
+
+    sched = soup.find("table", class_="dailySchedule")
+
+
+    consultants = dict()
+
+    num_rows = 0
+    col_index = 0
+
+    # There are 32 relevant rows in the schedule table
+    while(num_rows < 32):
+        # use BeautifulSoup to iterate through HTML elements. This steps through table rows and cells
+        sched = sched.find_next()
+
         # all th entries are the names of consultants as column headings
-        if a.name == "th":
-            num_consultants += 1
-            consultants.append(''.join(a.text.split())) # this line removes whitespace formatting
-            # print(consultants)
-            # print(len(consultants))
+        if sched.name == "th":
+            consultants[col_index] = dict()
+            consultants[col_index]["name"] = ''.join(sched.text.split()) # this removes whitespace formatting
+            consultants[col_index]["schedule"] = dict()
+            consultants[col_index]["schedule"][date] = set()
+            col_index += 1
 
-        elif a.name == "tr":
-            count = 0
+        # this is a new row in the table, so reset the cell index
+        elif sched.name == "tr":
+            col_index = 0
+            num_rows += 1
 
-        elif a.name == "td":
-            count += 1
-            # print(a.attrs)
+        # this is a new table cell
+        elif sched.name == "td":
             try:
-                if a['title'] != "" and "Preference" not in a['title'] and "Unavailable" not in a['title'] and "Class" not in a['title']:
-                    print(consultants[count-2], a['title'])
-                    # print(a['title'])
+                if sched['title'] != "" and "Preference" not in sched['title'] and "Unavailable" not in sched['title'] and "Class" not in sched['title']:
+                    consultants[col_index]["schedule"][date].add(sched['title'])
             except KeyError:
                 pass
+
+            col_index += 1
+
+    print(consultants[27])
 
 
     # print(soup.prettify())
